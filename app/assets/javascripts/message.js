153aaf1bbc95923cdaed3ec59caef87dd73c1759
daisuke0311 <58648817+daisuke0311@users.noreply.github.com>
2020-02-15 17:41:12 +0900

    不要削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,7 +52,6 @@ $('#new_message').on('submit', function(e){
    contentType: false
  })
   .done(function(data){
-  console.log(data.image)
     var html = buildHTML(data);
     $('.chat__message-list').append(html);      
        $('form')[0].reset();
