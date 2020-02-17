9db1de15465e3c21701527f49da85320d382a9f9
daisuke0311 <58648817+daisuke0311@users.noreply.github.com>
2020-02-17 17:24:26 +0900

    終了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(function(){ 
+  console.log("aaa")
   function buildHTML(message){
    if ( message.image ) {
      var html =
@@ -16,10 +16,10 @@ $(function(){
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} >
+         <img src=${message.image} class="lower-message__image" >
        </div>`
      return html;
-   } else {
+   } else if (message.content){
      var html =
       `<div class="chat__message-list-box" data-message-id=${message.id}>
          <div class="chat__message-list-box-info">
@@ -36,27 +36,72 @@ $(function(){
            </p>
          </div>
        </div>`
-     return html;
+       return html;
+   }else if (message.image){
+    `<div class="chat__message-list-box" data-message-id=${message.id}>
+         <div class="chat__message-list-box-info">
+           <p div class="chat__message-list-box-info-name">
+             ${message.user_name}
+           </p>
+           <p div class="chat__message-list-box-info-date">
+             ${message.created_at}
+           </p>
+         </div>
+         <div class="lower-message">
+         <img src=${message.image}class="lower-message__image" >
+         </div>
+       </div>`
    };
+   return html;
  }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
+ $(function(){ 
+  var reloadMessages = function() {
+    last_message_id = $('.chat__message-list-box:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log(messages)
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.chat__message-list').append(insertHTML);
+      $('.chat__message-list').animate({ scrollTop: $('.chat__message-list')[0].scrollHeight});
+    }
+  })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  $('#new_message').on('submit', function(e){
+    
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   .done(function(data){
+    // console.log(data)
     var html = buildHTML(data);
     $('.chat__message-list').append(html);      
        $('form')[0].reset();
-       $('.chat__message-list').animate({ scrollTop: $('.chat__message-list')[0].scrollHeight});
+       $('.chat__message-list').animate({ scrollTop: $('.chat__message-list')[0].scrollHeight},'fast');
        $('.submit-btn').prop('disabled', false);
+    })
   })
-})
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
