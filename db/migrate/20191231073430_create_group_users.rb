5e3c03c1f33c3b8feadb8fd73a63d60f28f557d6
daisuke0311 <58648817+daisuke0311@users.noreply.github.com>
2020-02-21 15:42:00 +0900

    Update 20191231073430_create_group_users.rb

--- db/migrate/20191231073430_create_group_users.rb
+++ db/migrate/20191231073430_create_group_users.rb
@@ -3,7 +3,7 @@ class CreateGroupUsers < ActiveRecord::Migration[5.0]
     create_table :group_users do |t|
 
       t.timestamps
-      t.string :name, null: false
+      # t.string :name, null: false
       t.references :group, foreign_key: true
       t.references :user,  foreign_key: true
     end
